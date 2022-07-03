a6b29d78d73550da7679cd1002236f26556ca219
Shourjjo_Majumder <shourjjomajum@outlook.com>
2022-07-03 15:43:16 +0530

    Organised the folders

--- src/what-lang.c
+++ src/what-lang.c
@@ -3,16 +3,15 @@
 #include <stdio.h>
 #include <string.h>
 
-
 void main( int argc, char *argv[]) {
     FILE *fp = fopen(argv[1],"r");
   char line[BUFSIZ];
   while (fgets(line, sizeof line, fp)) {
-    if (strstr(line, "tell:")) {
+  if (strstr(line, "tell:")) {
       fgets(line, sizeof line, fp);
      fputs(line, stdout);
     }
 
-    
+
   }
 }
