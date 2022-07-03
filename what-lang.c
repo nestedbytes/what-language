41b60078cf0740cb97f6474fb7d5f0e81f820330
Shourjjo_Majumder <shourjjomajum@outlook.com>
2022-07-03 13:28:06 +0530

    Initial Commit

---
+++ what-lang.c
@@ -0,0 +1,18 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+void main( int argc, char *argv[]) {
+    FILE *fp = fopen(argv[1],"r");
+  char line[BUFSIZ];
+  while (fgets(line, sizeof line, fp)) {
+    if (strstr(line, "tell:")) {
+      fgets(line, sizeof line, fp);
+      fputs(line, stdout);
+    }
+
+    
+  }
+}
