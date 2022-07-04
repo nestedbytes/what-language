25eac6ef0fa1d5ba57ece1cbada0deb107919f18
Shourjjo_Majumder <shourjjomajum@outlook.com>
2022-07-04 19:55:39 +0530

    Made the autoupdater better

--- src/what-lang.c
+++ src/what-lang.c
@@ -3,16 +3,51 @@
 #include <stdio.h>
 #include <string.h>
 
-void main( int argc, char *argv[]) {
-    FILE *fp = fopen(argv[1],"r");
+	
+
+void main( int argc, char **argv) {
+if (strcmp(argv[1], "--update") == 0)
+ 
+	
+     system("powershell Invoke-WebRequest https://raw.githubusercontent.com/what-language/interpreter/main/version.txt -OutFile C:/Windows/Temp/version.txt");
+  FILE *f2 = fopen("C:/Windows/Temp/version.txt","r");
+
   char line[BUFSIZ];
+  while (fgets(line, sizeof line, f2)) {
+  if (strstr(line, "1.1.1")) {
+printf("You are in correct version !\n");
+
+  }
+else{
+printf("Outdated ! Go to https://github.com/what-language/interpreter/releases");
+system("start https://github.com/what-language/interpreter/releases");
+
+}
+
+
+
+
+
+ 
+  printf("What-Language Interpreter \n");
+    printf("OutPut: \n");
+ FILE *fp = fopen(argv[1],"r");
+
   while (fgets(line, sizeof line, fp)) {
   if (strstr(line, "tell:")) {
       fgets(line, sizeof line, fp);
      fputs(line, stdout);
     }
+
    
 
-
+  
   }
+
+
+ 
 }
+}
+
+
+
