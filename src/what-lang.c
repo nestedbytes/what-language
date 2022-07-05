00094fc0515b9e022b196c3ca6661c8b56c6f5f0
Shourjjo_Majumder <shourjjomajum@outlook.com>
2022-07-05 19:25:05 +0530

    Version == 2.0.0

--- src/what-lang.c
+++ src/what-lang.c
@@ -6,15 +6,15 @@
 	
 
 void main( int argc, char **argv) {
-if (strcmp(argv[1], "--update") == 0)
+if (strcmp(argv[1], "--update") == 0) {
  
 	
-     system("powershell Invoke-WebRequest https://raw.githubusercontent.com/what-language/interpreter/main/version.txt -OutFile C:/Windows/Temp/version.txt");
-  FILE *f2 = fopen("C:/Windows/Temp/version.txt","r");
+     system("powershell Invoke-WebRequest https://raw.githubusercontent.com/what-language/interpreter/main/version.txt -OutFile C:/Windows/Temp/wlversion.txt");
+  FILE *f2 = fopen("C:/Windows/Temp/wlversion.txt","r");
 
   char line[BUFSIZ];
   while (fgets(line, sizeof line, f2)) {
-  if (strstr(line, "1.1.1")) {
+  if (strstr(line, "2.0.0")) {
 printf("You are in correct version !\n");
 
   }
@@ -23,31 +23,39 @@ printf("Outdated ! Go to https://github.com/what-language/interpreter/releases")
 system("start https://github.com/what-language/interpreter/releases");
 
 }
+  }
+}
+
 
 
 
 
 
  
-  printf("What-Language Interpreter \n");
-    printf("OutPut: \n");
- FILE *fp = fopen(argv[1],"r");
 
+ FILE *fp = fopen(argv[1],"r");
+ char line[BUFSIZ];
   while (fgets(line, sizeof line, fp)) {
   if (strstr(line, "tell:")) {
       fgets(line, sizeof line, fp);
      fputs(line, stdout);
-    }
+  }
+   if (strstr(line, "shell:")) {
+            fgets(line, sizeof(line), fp);
+         
+system(line);
+}
+
 
    
 
   
   }
 
-
  
 }
-}
+
+
 
 
 
